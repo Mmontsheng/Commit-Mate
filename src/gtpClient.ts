96152a3b8af145adb89c3126e339ca6d079971a5
Mmontsheng Maoto <mmontshengmaoto@gmail.com>
2023-01-11 18:22:58 +0200

    wip

---
+++ src/gtpClient.ts
@@ -0,0 +1,35 @@
+import { Configuration, OpenAIApi } from "openai";
+
+export class GptClient{
+  openai : any;
+  constructor(apiKey: string) {
+     const configuration = new Configuration({
+      apiKey,
+    });
+    this.openai = new OpenAIApi(configuration);
+  }
+  async getMessages(branchName: string) {
+    const prompt = `generate five git commit messages based on the following files that changed: ${branchName}`
+    try {
+      const response = await this.openai.createCompletion({
+        model: "text-davinci-003",
+        prompt,
+        temperature: 0.7,
+        max_tokens: 256,
+        top_p: 1,
+        frequency_penalty: 0.2,
+        presence_penalty: 0,
+      });
+      console.log(response.data.choices[0].text.split(/\r?\n/).filter((t: string) => t?.length));
+      
+    } catch (error: any) {
+      if (error.response) {
+        console.log(error.response.status);
+        console.log(error.response.data);
+      } else {
+        console.log(error.message);
+      }
+        
+    }
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-console.log('sdk');
+#!/usr/bin/env node
+import 'dotenv/config'
+import { execSync } from 'child_process';
+
+import { GptClient } from './gtpClient';
+
+const apiKey = process.env.GPT_API_KEY || '';
+if(!apiKey || !apiKey.length) {
+  console.log('no api key, please define api key');
+}
+
+const gtpClient = new GptClient(apiKey);
+let branch = '';
+try {
+  branch = execSync('git diff --name-only').toString();
+  if (!branch) {
+    console.log('No changes to commit.');
+    process.exit(0);
+  }
+} catch (e) {
+  console.log('Failed to run git diff --cached');
+  process.exit(1);
+}
+// console.log(diff);
+
+gtpClient.getMessages(branch);
